6f17398ba06c27c4e0d00cff53a1163f6cc38925
Eric Biggers <ebiggers3@gmail.com>
2016-06-02 20:00:22 -0500

    Rewrite string encoding conversion code
    - Only support UTF-8 on UNIX and UTF-16LE on Windows
    - Don't rely on iconv

--- include/wimlib/encoding.h
+++ include/wimlib/encoding.h
@@ -7,85 +7,49 @@
 #include "wimlib/util.h"
 #include "wimlib/types.h"
 
-extern void
-iconv_global_init(void);
+extern int
+utf8_to_utf16le(const char *in, size_t in_nbytes,
+		utf16lechar **out_ret, size_t *out_nbytes_ret);
 
-extern void
-iconv_global_cleanup(void);
-
-extern u16 upcase[65536];
-
-extern void
-init_upcase(void);
-
-extern bool wimlib_mbs_is_utf8;
-
-#define DECLARE_CHAR_CONVERSION_FUNCTIONS(varname1, varname2,		\
-					  chartype1, chartype2)		\
-									\
-extern int								\
-varname1##_to_##varname2(const chartype1 *in, size_t in_nbytes,		\
-			 chartype2 **out_ret,				\
-			 size_t *out_nbytes_ret);			\
-									\
-extern int								\
-varname1##_to_##varname2##_nbytes(const chartype1 *in, size_t in_nbytes,\
-				  size_t *out_nbytes_ret);		\
-									\
-extern int								\
-varname1##_to_##varname2##_buf(const chartype1 *in, size_t in_nbytes,	\
-			       chartype2 *out);
-
-extern utf16lechar *
-utf16le_dupz(const void *ustr, size_t usize);
-
-extern utf16lechar *
-utf16le_dup(const utf16lechar *ustr);
-
-extern size_t
-utf16le_len_bytes(const utf16lechar *s);
-
-extern size_t
-utf16le_len_chars(const utf16lechar *s);
-
-#if !TCHAR_IS_UTF16LE
-DECLARE_CHAR_CONVERSION_FUNCTIONS(utf16le, tstr, utf16lechar, tchar);
-DECLARE_CHAR_CONVERSION_FUNCTIONS(tstr, utf16le, tchar, utf16lechar);
-#else
+extern int
+utf16le_to_utf8(const utf16lechar *in, size_t in_nbytes,
+		char **out_ret, size_t *out_nbytes_ret);
 
 static inline int
-tstr_to_utf16le(const tchar *tstr, size_t tsize,
-		utf16lechar **ustr_ret, size_t *usize_ret)
+tstr_to_tstr(const tchar *in, size_t in_nbytes,
+	     tchar **out_ret, size_t *out_nbytes_ret)
 {
-	utf16lechar *ustr = utf16le_dupz(tstr, tsize);
-	if (!ustr)
+	*out_ret = MALLOC(in_nbytes + sizeof(tchar));
+	if (unlikely(!*out_ret))
 		return WIMLIB_ERR_NOMEM;
-	*ustr_ret = ustr;
-	*usize_ret = tsize;
+	memcpy(*out_ret, in, in_nbytes);
+	(*out_ret)[in_nbytes / sizeof(tchar)] = 0;
+	if (out_nbytes_ret)
+		*out_nbytes_ret = in_nbytes;
 	return 0;
 }
 
-#define utf16le_to_tstr tstr_to_utf16le
+#if TCHAR_IS_UTF16LE
+
+/* tstr(UTF-16LE) <=> UTF-16LE  */
+#  define tstr_to_utf16le	tstr_to_tstr
+#  define utf16le_to_tstr	tstr_to_tstr
+
+/* tstr(UTF-16LE) <=> UTF-8  */
+#  define tstr_to_utf8		utf16le_to_utf8
+#  define utf8_to_tstr		utf8_to_utf16le
+
+#else
+
+/* tstr(UTF-8) <=> UTF-16LE  */
+#  define tstr_to_utf16le	utf8_to_utf16le
+#  define utf16le_to_tstr	utf16le_to_utf8
+
+/* tstr(UTF-8) <=> UTF-8  */
+#  define tstr_to_utf8		tstr_to_tstr
+#  define utf8_to_tstr		tstr_to_tstr
 
 #endif
-
-DECLARE_CHAR_CONVERSION_FUNCTIONS(utf8, tstr, char, tchar);
-DECLARE_CHAR_CONVERSION_FUNCTIONS(tstr, utf8, tchar, char);
-
-extern int
-utf8_to_tstr_simple(const char *utf8str, tchar **out);
-
-extern int
-tstr_to_utf8_simple(const tchar *tstr, char **out);
-
-extern int
-cmp_utf16le_strings(const utf16lechar *s1, size_t n1,
-		    const utf16lechar *s2, size_t n2,
-		    bool ignore_case);
-
-extern int
-cmp_utf16le_strings_z(const utf16lechar *s1, const utf16lechar *s2,
-		      bool ignore_case);
 
 /* Convert a string in the platform-dependent encoding to UTF-16LE, but if both
  * encodings are UTF-16LE, simply re-use the string.  Release with
@@ -117,8 +81,7 @@ tstr_get_utf16le(const tchar *tstr, const utf16lechar **ustr_ret)
 	return 0;
 #else
 	size_t tsize = tstrlen(tstr) * sizeof(tchar);
-	size_t dummy;
-	return tstr_to_utf16le(tstr, tsize, (utf16lechar **)ustr_ret, &dummy);
+	return tstr_to_utf16le(tstr, tsize, (utf16lechar **)ustr_ret, NULL);
 #endif
 }
 
@@ -142,7 +105,8 @@ utf16le_get_tstr(const utf16lechar *ustr, size_t usize,
 #if TCHAR_IS_UTF16LE
 	/* No conversion or copy needed  */
 	*tstr_ret = ustr;
-	*tsize_ret = usize;
+	if (tsize_ret)
+		*tsize_ret = usize;
 	return 0;
 #else
 	return utf16le_to_tstr(ustr, usize, (tchar **)tstr_ret, tsize_ret);
@@ -157,5 +121,34 @@ utf16le_put_tstr(const tchar *tstr)
 	FREE((void *)tstr);
 #endif
 }
+
+
+/* UTF-16LE utility functions  */
+
+extern u16 upcase[65536];
+
+extern void
+init_upcase(void);
+
+extern int
+cmp_utf16le_strings(const utf16lechar *s1, size_t n1,
+		    const utf16lechar *s2, size_t n2,
+		    bool ignore_case);
+
+extern int
+cmp_utf16le_strings_z(const utf16lechar *s1, const utf16lechar *s2,
+		      bool ignore_case);
+
+extern utf16lechar *
+utf16le_dupz(const void *ustr, size_t usize);
+
+extern utf16lechar *
+utf16le_dup(const utf16lechar *s);
+
+extern size_t
+utf16le_len_bytes(const utf16lechar *s);
+
+extern size_t
+utf16le_len_chars(const utf16lechar *s);
 
 #endif /* _WIMLIB_ENCODING_H */
